45529cf65fd73ea1814100523b616eafa262c44f
Ashish Sengar <65999078+Ashishss21@users.noreply.github.com>
2021-05-18 22:11:23 +0530

    Production- Test-1

--- src/MainComponent.js
+++ src/MainComponent.js
@@ -1,25 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Todo from "./components/Todo";
 import "./App.css";
+import db from "./firebase";
 import Bottom from "./components/Bottom";
 import { Container } from "react-bootstrap";
-import AddModal from "./components/AddTask";
+import AddModal from './components/AddTask';
 
 function Main() {
   const [todos, setTodos] = useState([]);
+  // const [input, setInput] = useState("");
+
+  //when the app koads, we need to listen to the database and fetch new todos as the get added/removed
+  useEffect(() => {
+    //this code here.... fires when the app.js loads
+    db.collection("todos")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setTodos(
+          snapshot.docs.map((doc) => ({ id: doc.id, todo: doc.data().todo }))
+        );
+      });
+  }, []);
 
   return (
     <>
-      {/* <div class="card" style={{width: '18rem'}}>
+      {/* <TopNavbar/> */}
+      <div class="card">
         <div class="card-body">
-          <AddModal />
+          <h5 class="card-title">Add Task</h5>
+          <AddModal/>
         </div>
-      </div> */}
+      </div>
       {todos.map((todo) => (
         <Container>
           <Todo todo={todo} />
         </Container>
       ))}
+
       <Bottom />
     </>
   );
